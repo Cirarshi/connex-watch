75253c71f06611954d1783139917eb8ef045a0d0
Utkarsh Kapoor <98.utkarsh.kapoor@gmail.com>
2025-09-23 13:14:10 +0530

    removed all so far

--- backend/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-exports.loginUser = (req, res) => {
-  const { email, password } = req.body;
-  // Dummy auth
-  if (email === "test@example.com" && password === "password") {
-    return res.json({ token: "fake-jwt-token" });
-  }
-  return res.status(401).json({ message: "Invalid credentials" });
-};
